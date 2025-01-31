46441354e3ce57fbb9b842a446b6d59195e5cec3
AlinaGay <gayfullina.a@gmail.com>
2025-01-31 07:26:47 +0100

    Add constants

--- news/tests/test_trial.py
+++ news/tests/test_trial.py
@@ -1,31 +1,22 @@
-# news/tests/test_trial.py
 from django.test import TestCase
 
-# Импортируем модель, чтобы работать с ней в тестах.
 from news.models import News
 
 
-# Создаём тестовый класс с произвольным названием, наследуем его от TestCase.
 class TestNews(TestCase):
+    TITLE = 'Заголовок новости'
+    TEXT = 'Текстовый текст'
 
-    # В методе класса setUpTestData создаём тестовые объекты.
-    # Оборачиваем метод соответствующим декоратором.    
     @classmethod
-    def setUpTestData(cls):
-        # Стандартным методом Django ORM create() создаём объект класса.
-        # Присваиваем объект атрибуту класса: назовём его news.
+    def setUpClass(cls):
         cls.news = News.objects.create(
-            title='Заголовок новости',
-            text='Тестовый текст',
+            title=cls.TITLE,
+            text=cls.TEXT
         )
 
-    # Проверим, что объект действительно был создан.
     def test_successful_creation(self):
-        # При помощи обычного ORM-метода посчитаем количество записей в базе.
         news_count = News.objects.count()
-        # Сравним полученное число с единицей.
         self.assertEqual(news_count, 1)
 
     def test_title(self):
-        # Сравним свойство объекта и ожидаемое значение.
-        self.assertEqual(self.news.title, 'Заголовок новости')     
+        self.assertEqual(self.news.title, self.TITLE)        

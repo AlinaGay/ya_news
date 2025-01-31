81e3af7dc1a3b00fbbb238f9073186460171074d
AlinaGay <gayfullina.a@gmail.com>
2025-01-31 08:41:30 +0100

    Add changes

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
 from django.test import TestCase
+
+from django.urls import reverse
 
 
 class TestRoutes(TestCase):
 
     def test_home_page(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        url = reverse('news:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)

99e07092233657aaa7cac80259ee954d9d8e3de6
AlinaGay <gayfullina.a@gmail.com>
2025-02-01 07:31:57 +0100

    Add bulk_create

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -8,5 +8,8 @@ class TestHomePage(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
+        all_news = [
             News.objects.create(title=f'Новость {index}', text='Просто текст.')
+            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+        ]
+        News.objects.bulk_create(all_news)

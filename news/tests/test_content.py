ffe2f1b2578cf91476ce84472a64306c3a39dfe6
AlinaGay <gayfullina.a@gmail.com>
2025-02-01 11:48:54 +0100

    Correct mistakes

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,15 +1,24 @@
 from django.conf import settings
 from django.test import TestCase
+from django.urls import reverse
 
 from news.models import News
 
 
 class TestHomePage(TestCase):
 
+    HOME_URL = reverse('news:home')
+
     @classmethod
     def setUpTestData(cls):
         all_news = [
-            News.objects.create(title=f'Новость {index}', text='Просто текст.')
+            News(title=f'Новость {index}', text='Просто текст.')
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         ]
         News.objects.bulk_create(all_news)
+
+    def test_news_count(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        news_count = object_list.count()
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)

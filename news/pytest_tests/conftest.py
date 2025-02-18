cee39da4a94ae34068728d5c90dfe8e47fb3323c
AlinaGay <gayfullina.a@gmail.com>
2025-02-18 11:15:14 +0100

    Add test_availability_for_edit_and_delete_of_comment

---
+++ news/pytest_tests/conftest.py
@@ -0,0 +1,58 @@
+import pytest
+
+from django.test.client import Client
+
+from news.models import News, Comment
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):
+    client = Client()
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)
+    return client
+
+
+@pytest.fixture
+def news(author):
+    news = News.objects.create(
+        title='Заголовок',
+        text='Текст'
+    )
+    return news
+
+
+@pytest.fixture
+def id_for_news(news):
+    return (news.id,)
+
+
+@pytest.fixture
+def comment(author, news):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def id_for_comment(comment):
+    return (comment.id,)
--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -4,3 +4,43 @@ from django.urls import reverse
 from pytest_django.asserts import assertRedirects
 
 import pytest
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('news:home', None),
+        ('news:detail', pytest.lazy_fixture('id_for_news')),
+        ('users:login', None),
+        ('users:logout', None),
+        ('users:signup', None),
+    ),
+)
+def test_pages_availability_for_anonymous_user(not_author_client, name, args):
+    url = reverse(name, args=args)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('not_author_client'),HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('news:edit', pytest.lazy_fixture('id_for_comment')),
+        ('news:delete', pytest.lazy_fixture('id_for_comment')),
+    ),
+)
+def test_availability_for_edit_and_delete_of_comment(parametrized_client,
+                                                name,
+                                                args,
+                                                expected_status):
+    url = reverse(name, args=args)
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status

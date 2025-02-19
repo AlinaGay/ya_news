abd57468685eb353159bf03ad00266534f44814a
AlinaGay <gayfullina.a@gmail.com>
2025-02-19 16:48:33 +0100

    Add test_comments_order

--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -21,3 +21,12 @@ def test_news_order(client, news_data):
     sorted_dates = sorted(all_dates, reverse=True)
     assert all_dates == sorted_dates
 
+
+def test_comments_order(client, id_for_news):
+    url = reverse('news:detail', args=id_for_news)
+    response = client.get(url)
+    news = response.context['news']
+    all_comments = news.comment_set.all()
+    all_timestamps = [comment.created for comment in all_comments]
+    sorted_timestamps = sorted(all_timestamps)
+    assert all_timestamps == sorted_timestamps

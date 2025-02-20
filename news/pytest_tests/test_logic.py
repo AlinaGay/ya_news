bf83ee37a25436df3724072933d2f2755282297f
AlinaGay <gayfullina.a@gmail.com>
2025-02-20 13:50:53 +0100

    Add test_user_can_create_comment

--- news/pytest_tests/test_logic.py
+++ news/pytest_tests/test_logic.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
+from pytest_django.asserts import assertRedirects
 
 from news.forms import BAD_WORDS, WARNING
 from news.models import Comment, News
@@ -16,3 +17,22 @@ def test_anonymous_user_cant_create_comment(
     client.post(url, data=comment_form_data)
     comments_count = Comment.objects.count()
     assert comments_count == 0
+
+
+def test_user_can_create_comment(
+        author,
+        author_client,
+        id_for_news,
+        comment_form_data):
+    url = reverse('news:detail', args=id_for_news)
+    response = author_client.post(url, data=comment_form_data)
+    comments_count = Comment.objects.count()
+    comment = Comment.objects.get()
+    news_from_db = News.objects.get()
+
+    assertRedirects(response, f'{url}#comments')
+    assert comments_count == 1
+    assert comment.text == comment_form_data['text']
+    assert comment.news == news_from_db
+    assert comment.author == author
+
